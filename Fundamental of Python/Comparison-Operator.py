ca9fea604c01e2bc3711ae20865f0fef52ae21f6
rushabhstridhyatech <200303108718@paruluniversity.ac.in>
2024-03-16 18:09:56 +0530

    Comparison Operator

---
+++ Fundamental of Python/Comparison-Operator.py
@@ -0,0 +1,8 @@
+# There are total six comparison Operator.
+    #  1 ) Eqaul   ==
+    #  2 ) Not Equal  !=
+    #  3 ) Greater Then  >
+    #  4 ) Greater Then Equal to 
+    #  5 ) Less Then Operator <
+    #  6 ) Less then Equal TP <=
+
