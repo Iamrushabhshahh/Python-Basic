1678784cb2021d7934a5a64f596ccb18d87fe2c3
rushabhstridhyatech <200303108718@paruluniversity.ac.in>
2024-03-16 16:28:15 +0530

    Comments Explanation

---
+++ Fundamental of Python/Comments.py
@@ -0,0 +1,5 @@
+# Commentsare useful for humans only
+# A commnt is created by a #  followed by a text
+# A multi-line comment should have a # in front of every line
+# Don't write unnecessary comments write self documenting code instead.
+
