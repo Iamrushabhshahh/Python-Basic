9650793a4290c1aa87f7f0298237f7d009a2544e
rushabhstridhyatech <200303108718@paruluniversity.ac.in>
2024-03-16 16:20:20 +0530

    Variables.py

---
+++ Fundamental of Python/variables.py
@@ -0,0 +1,23 @@
+# Variables allow us to store value
+# A variable has a vlaid name ( letter,underscore,digits, not a reserved keyword)
+# Python is dynamically typed so variables can be re declared.
+# We can use shortcut operator in order to cleanly redeclare a variable
+
+number_of_tomato = 5
+number_of_potato = 15
+
+print("Total of Item" , number_of_potato + number_of_tomato)
+
+# You can also redeclare value like 
+
+number_of_potato = number_of_potato + 7
+print (number_of_potato)
+
+number_of_potato = 50
+print(number_of_potato)
+
+#  Shortcut variable lik
+
+number_of_tomato += 1
+print(number_of_tomato)
+
