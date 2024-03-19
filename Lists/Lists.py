ec93866bd0eb6461ec4e8fc4b924c63309117d27
rushabhstridhyatech <200303108718@paruluniversity.ac.in>
2024-03-20 00:44:15 +0530

    List

---
+++ Lists/Lists.py
@@ -0,0 +1,23 @@
+# if you want to have the collection of the multiple elements we creat the list
+# each element is a scaler
+# the very first element has a index 0 and then 1 then 2 etc...
+# if you want any specific element in list we mention the index in sqar bracket.abs
+
+countries = ["India","Usa","Africa","Indonesia"]
+# for i in range(len(countries)):
+#     print(countries[i])
+
+# Print By index
+print(countries[0])
+print(countries[1])
+
+
+# Enty index by 
+countries[3] = "Rushabh"
+
+# Check len by pre define function
+len(countries)
+
+# Delete by index 
+del countries[1]
+
