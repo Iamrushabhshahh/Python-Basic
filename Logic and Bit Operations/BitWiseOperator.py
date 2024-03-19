a86a0837bed2b01a6101549bcfc9075baa1877c7
rushabhstridhyatech <200303108718@paruluniversity.ac.in>
2024-03-20 00:28:12 +0530

    BitWise Operations

---
+++ Logic and Bit Operations/BitWiseOperator.py
@@ -0,0 +1,32 @@
+# IT ALLOWS YOU TO MANUPILATE SINGLE BIT OF DATA
+#   List of Bitwise Operator
+#     1 ) & - Conjuction
+#     2 ) | - Disjunction 
+#     3 ) ~ - Negation
+#     4 ) ^ - Exclusie
+
+
+
+#     1 ) & - Conjuction
+
+print(15 & 22)
+
+#     2 ) | - Disjunction 
+
+print(15 | 22)
+
+#     4 ) ^ - Exclusie
+
+print(15 ^ 22) 
+
+#     3 ) ~ - Negation
+
+print(~22)
+
+
+#  Bit Shifting 
+# >> Shifting right
+#  << Shifting Left
+
+print( 22 >> 1 )
+print( 22 << 1 )
